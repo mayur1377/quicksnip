8f5fac7d4b6f26c9e4246e44b8d73519cb43556c
MAYUR <mayuresh1377@gmail.com>
2025-01-03 14:45:44 +0530

    feat: added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,6 +7,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
